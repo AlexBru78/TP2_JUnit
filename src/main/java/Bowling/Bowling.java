9eb680129bed8614aac891a8ef9f9e83322e8b7f
Alexandre Bruniaux <alex.bruniaux@gmail.com>
2022-05-04 23:29:57 +0200

    Code réalisé pour la classe Bowling et partiellement pour les tests de la classe TestBowling

--- src/main/java/Bowling/Bowling.java
+++ src/main/java/Bowling/Bowling.java
@@ -1,4 +1,42 @@
 package Bowling;
 
 public class Bowling {
+
+    /*public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }*/
+    /*https://www.bowlinggenius.com/*/
+
+    public int move_spare(int ball1, int after_ball_score) {
+        int score = ball1 + (10 - ball1);
+        score += after_ball_score;
+        return score;
+    }
+
+    public int move_strike(int first_bonus_ball, int second_bonus_ball) {
+        int score = 10;
+        score += first_bonus_ball + second_bonus_ball;
+        return score;
+    }
+
+    public int bad_move(int ball1, int ball2) {
+        int score = ball1 + ball2;
+        return score;
+    }
+
+    public int final_frame_strike(int ball1_bonus, int ball2_bonus) {
+        int score = 10;
+        score += ball1_bonus + ball2_bonus;
+        return score;
+    }
+
+    public int final_frame_spare(int ball_bonus) {
+        int score = 10;
+        score += ball_bonus;
+        return score;
+    }
+
+    public int final_frame_bad(int score) {
+        return score;
+    }
 }
--- src/test/java/TestBowling.java
+++ src/test/java/TestBowling.java
@@ -1,2 +1,37 @@
+import Bowling.Bowling;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestBowling {
+    Bowling game = new Bowling();
+    int final_score = 0;
+    @Test
+    public void TestGame(){
+        final_score += game.bad_move(5, 4);
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(9, 0);
+        final_score += game.move_spare(9,3);
+        final_score += game.bad_move(3, 6);
+        final_score += game.move_spare(5,10);
+        final_score += game.final_frame_strike(10,10);
+        Assertions.assertEquals(220,final_score);
+
+    }
+    @Test
+    public void StrikeGame(){
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(10, 10);
+        final_score += game.move_strike(10, 10);
+        final_score += game.final_frame_strike(10,10);
+        Assertions.assertEquals(300,final_score);
+    }
 }
