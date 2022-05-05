1dfb0b355bc0706e8eb6daecb180db583d83f3dd
Alexandre Bruniaux <alex.bruniaux@gmail.com>
2022-05-05 20:43:02 +0200

    Exo 2 finished

--- src/main/java/Bowling/Bowling.java
+++ src/main/java/Bowling/Bowling.java
@@ -2,41 +2,64 @@ package Bowling;
 
 public class Bowling {
 
-    /*public int getRandomNumber(int min, int max) {
-        return (int) ((Math.random() * (max - min)) + min);
-    }*/
-    /*https://www.bowlinggenius.com/*/
-
-    public int move_spare(int ball1, int after_ball_score) {
+    public int move_spare(int ball1, int bonus_ball) {
+        /*@param :
+        ball1 : score of the first ball
+        bonus_ball : bonus score of the spare
+        @return : sum of the spare score and bonus score of the next ball
+        */
         int score = ball1 + (10 - ball1);
-        score += after_ball_score;
+        score += bonus_ball;
         return score;
     }
 
     public int move_strike(int first_bonus_ball, int second_bonus_ball) {
+        /*@param :
+        first_bonus_ball : score of the first bonus ball
+        second_bonus_ball : score of the second bonus ball
+        @return : sum of the strike score and two bonus ball of the next two ball
+        */
         int score = 10;
         score += first_bonus_ball + second_bonus_ball;
         return score;
     }
 
     public int bad_move(int ball1, int ball2) {
+        /*@param :
+        ball1 : score of the first ball
+        ball2 : score of the second ball
+        @return : sum of the two balls
+        */
         int score = ball1 + ball2;
         return score;
     }
 
     public int final_frame_strike(int ball1_bonus, int ball2_bonus) {
+        /*@param :
+        ball1_bonus : score of the first bonus ball
+        ball2_bonus : score of the second bonus ball
+        @return : sum of the two bonus balls and the strike of the last frame
+        */
         int score = 10;
         score += ball1_bonus + ball2_bonus;
         return score;
     }
 
     public int final_frame_spare(int ball_bonus) {
+        /*@param :
+        ball_bonus : score of the first ball
+        @return : sum of the bonus ball and the spare of the last frame
+        */
         int score = 10;
         score += ball_bonus;
         return score;
     }
 
     public int final_frame_bad(int score) {
+        /*@param :
+        score : score of the ball
+        @return : score of the ball
+        */
         return score;
     }
 }
--- src/test/java/TestBowling.java
+++ src/test/java/TestBowling.java
@@ -6,7 +6,7 @@ public class TestBowling {
     Bowling game = new Bowling();
     int final_score = 0;
     @Test
-    public void TestGame(){
+    public void TestRandomGame1(){
         final_score += game.bad_move(5, 4);
         final_score += game.move_strike(10, 10);
         final_score += game.move_strike(10, 10);
@@ -20,6 +20,24 @@ public class TestBowling {
         Assertions.assertEquals(220,final_score);
 
     }
+
+    @Test
+    public void TestRandomGame2(){
+        final_score += game.bad_move(3, 5);
+        final_score += game.move_spare(3,5);
+        final_score += game.bad_move(5, 4);
+        final_score += game.move_spare(6,8);
+        final_score += game.move_spare(8,4);
+        final_score += game.bad_move(4, 2);
+        final_score += game.bad_move(5, 3);
+        final_score += game.bad_move(5, 2);
+        final_score += game.bad_move(5, 1);
+        final_score += game.final_frame_spare(5);
+        Assertions.assertEquals(106,final_score);
+
+    }
+
+
     @Test
     public void StrikeGame(){
         final_score += game.move_strike(10, 10);
@@ -34,4 +52,20 @@ public class TestBowling {
         final_score += game.final_frame_strike(10,10);
         Assertions.assertEquals(300,final_score);
     }
+
+    @Test
+    public void BadGame(){
+        final_score += game.bad_move(1, 1);
+        final_score += game.bad_move(1, 1);
+        final_score += game.bad_move(1, 1);
+        final_score += game.bad_move(1, 1);
+        final_score += game.bad_move(1, 1);
+        final_score += game.bad_move(1, 1);
+        final_score += game.bad_move(1, 1);
+        final_score += game.bad_move(1, 1);
+        final_score += game.bad_move(1, 1);
+        final_score += game.final_frame_bad(2);
+        Assertions.assertEquals(20,final_score);
+    }
+
 }
