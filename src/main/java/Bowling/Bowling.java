df2fca79f73c4ee2a0f8490733a30f76e1ab75da
Alexandre Bruniaux <alex.bruniaux@gmail.com>
2022-05-09 17:22:33 +0200

    Super Correction incroyable

--- src/main/java/Bowling/Bowling.java
+++ src/main/java/Bowling/Bowling.java
@@ -1,32 +1,37 @@
 package Bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bowling {
 
     public int final_score;
+    List<Tour> tab_score;
 
-    public Bowling(int final_score) {
+    public Bowling(int final_score, List<Tour> tab_score) {
         this.final_score = final_score;
+        this.tab_score = tab_score;
     }
 
-    public void move_spare(int ball1, int bonus_ball) {
+    public void move_spare(int ball1) {
         /*@param :
         ball1 : score of the first ball
         bonus_ball : bonus score of the spare
         @return : sum of the spare score and bonus score of the next ball
         */
         int score = ball1 + (10 - ball1);
-        score += bonus_ball;
+        tab_score.add(new Tour(ball1, 10 - ball1, false, true));
         final_score += score;
     }
 
-    public void move_strike(int first_bonus_ball, int second_bonus_ball) {
+    public void move_strike() {
         /*@param :
         first_bonus_ball : score of the first bonus ball
         second_bonus_ball : score of the second bonus ball
         @return : sum of the strike score and two bonus ball of the next two ball
         */
         int score = 10;
-        score += first_bonus_ball + second_bonus_ball;
+        tab_score.add(new Tour(10, 0, true, false));
         final_score += score;
     }
 
@@ -37,6 +42,7 @@ public class Bowling {
         @return : sum of the two balls
         */
         int score = ball1 + ball2;
+        tab_score.add(new Tour(ball1, ball2, false, false));
         final_score += score;
     }
 
@@ -46,7 +52,7 @@ public class Bowling {
         ball2_bonus : score of the second bonus ball
         @return : sum of the two bonus balls and the strike of the last frame
         */
-        int score = 10;
+        int score = 0;
         score += ball1_bonus + ball2_bonus;
         final_score += score;
     }
@@ -67,5 +73,42 @@ public class Bowling {
         @return : score of the ball
         */
         final_score += score;
+    }
+
+    public void add_bonus_score(List<Tour> tab_score) {
+        int bonus = 0;
+        for (int i = 0; i < tab_score.size(); i++) {
+            if (tab_score.get(i).isStrike) {
+                bonus += get_strike_bonus(i);
+            }
+            if (tab_score.get(i).isSpare) {
+                bonus += get_spare_bonus(i);
+            }
+        }
+        final_score += bonus;
+    }
+
+    public int get_strike_bonus(int i) {
+        int bonus = 0;
+        if (i < tab_score.size() - 2) {
+            if (tab_score.get(i + 1).isStrike) {
+                bonus += tab_score.get(i + 1).first_move;
+                bonus += tab_score.get(i + 2).first_move;
+            } else {
+                bonus += tab_score.get(i + 1).first_move;
+                bonus += tab_score.get(i + 1).second_move;
+            }
+        } else if (i < tab_score.size() - 1) {
+            bonus += tab_score.get(i + 1).first_move;
+        }
+
+        return bonus;
+    }
+
+    public int get_spare_bonus(int i) {
+        if (i < tab_score.size() - 1) {
+            return tab_score.get(i + 1).first_move;
+        }
+        return 0;
     }
 }
--- src/test/java/TestBowling.java
+++ src/test/java/TestBowling.java
@@ -1,10 +1,14 @@
 import Bowling.Bowling;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import Bowling.Tour;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestBowling {
-    Bowling game = new Bowling(0);
-
+    List<Tour> tab_score = new ArrayList<Tour>();
+    Bowling game = new Bowling(0, tab_score);
+/*
     @Test
     public void TestRandomGame1() {
         game.bad_move(5, 4);
@@ -18,9 +22,60 @@ public class TestBowling {
         game.move_spare(5, 10);
         game.final_frame_strike(10, 10);
         Assertions.assertEquals(220, game.final_score);
-
+        System.out.println(tab_score);
+    }
+*/
+    @Test
+    public void TestRandomGame3() {
+        game.bad_move(5, 4);
+        game.move_strike();
+        game.move_strike();
+        game.move_strike();
+        game.move_strike();
+        game.move_strike();
+        game.move_spare(9);
+        game.bad_move(3, 6);
+        game.move_spare(5);
+        game.move_strike();
+        game.add_bonus_score(tab_score);
+        game.final_frame_strike(10, 10);
+        Assertions.assertEquals(220, game.final_score);
+        System.out.println(tab_score);
+    }
+    @Test
+    public void TestRandomGame1() {
+        game.move_strike();
+        game.move_strike();
+        game.move_strike();
+        game.move_strike();
+        game.move_strike();
+        game.move_strike();
+        game.move_strike();
+        game.move_strike();
+        game.move_strike();
+        game.move_strike();
+        game.add_bonus_score(tab_score);
+        game.final_frame_strike(10, 10);
+        Assertions.assertEquals(300, game.final_score);
+        System.out.println(tab_score);
     }
 
+    @Test
+    public void TestRandomGame2() {
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.add_bonus_score(tab_score);
+        game.final_frame_bad(2);
+        Assertions.assertEquals(20, game.final_score);
+    }
+/*
     @Test
     public void TestRandomGame2() {
         game.bad_move(3, 5);
@@ -67,5 +122,5 @@ public class TestBowling {
         game.final_frame_bad(2);
         Assertions.assertEquals(20, game.final_score);
     }
-
+*/
 }
