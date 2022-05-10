bbee78923bad2635314c635cc5c3baa20092e976
Aymeric <aymeric@brightfor.me>
2022-05-10 14:28:49 +0200

    feat: add bowling tests

--- src/test/java/TestBowling.java
+++ src/test/java/TestBowling.java
@@ -8,25 +8,9 @@ import java.util.List;
 public class TestBowling {
     List<Tour> tab_score = new ArrayList<Tour>();
     Bowling game = new Bowling(0, tab_score);
-/*
+
     @Test
-    public void TestRandomGame1() {
-        game.bad_move(5, 4);
-        game.move_strike(10, 10);
-        game.move_strike(10, 10);
-        game.move_strike(10, 10);
-        game.move_strike(10, 9);
-        game.move_strike(9, 1);
-        game.move_spare(9, 3);
-        game.bad_move(3, 6);
-        game.move_spare(5, 10);
-        game.final_frame_strike(10, 10);
-        Assertions.assertEquals(220, game.final_score);
-        System.out.println(tab_score);
-    }
-*/
-    @Test
-    public void TestRandomGame3() {
+    public void TestRandomGameStrikeLast() {
         game.bad_move(5, 4);
         game.move_strike();
         game.move_strike();
@@ -43,7 +27,7 @@ public class TestBowling {
         System.out.println(tab_score);
     }
     @Test
-    public void TestRandomGame1() {
+    public void TestGameAllStrike() {
         game.move_strike();
         game.move_strike();
         game.move_strike();
@@ -61,7 +45,7 @@ public class TestBowling {
     }
 
     @Test
-    public void TestRandomGame2() {
+    public void TestGameAllBad() {
         game.bad_move(1, 1);
         game.bad_move(1, 1);
         game.bad_move(1, 1);
@@ -75,52 +59,36 @@ public class TestBowling {
         game.final_frame_bad(2);
         Assertions.assertEquals(20, game.final_score);
     }
-/*
-    @Test
-    public void TestRandomGame2() {
-        game.bad_move(3, 5);
-        game.move_spare(3, 5);
-        game.bad_move(5, 4);
-        game.move_spare(6, 8);
-        game.move_spare(8, 4);
-        game.bad_move(4, 2);
-        game.bad_move(5, 3);
-        game.bad_move(5, 2);
-        game.bad_move(5, 1);
-        game.final_frame_spare(5);
-        Assertions.assertEquals(106, game.final_score);
-
-    }
-
 
     @Test
-    public void StrikeGame() {
-        game.move_strike(10, 10);
-        game.move_strike(10, 10);
-        game.move_strike(10, 10);
-        game.move_strike(10, 10);
-        game.move_strike(10, 10);
-        game.move_strike(10, 10);
-        game.move_strike(10, 10);
-        game.move_strike(10, 10);
-        game.move_strike(10, 10);
-        game.final_frame_strike(10, 10);
-        Assertions.assertEquals(300, game.final_score);
+    public void TestGameSpareLast() {
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.bad_move(1, 1);
+        game.add_bonus_score(tab_score);
+        game.final_frame_spare(1);
+        Assertions.assertEquals(29, game.final_score);
     }
 
     @Test
-    public void BadGame() {
-        game.bad_move(1, 1);
-        game.bad_move(1, 1);
-        game.bad_move(1, 1);
-        game.bad_move(1, 1);
-        game.bad_move(1, 1);
-        game.bad_move(1, 1);
-        game.bad_move(1, 1);
-        game.bad_move(1, 1);
-        game.bad_move(1, 1);
-        game.final_frame_bad(2);
-        Assertions.assertEquals(20, game.final_score);
+    public void TestGameAllSpare() {
+        game.move_spare(9);
+        game.move_spare(9);
+        game.move_spare(9);
+        game.move_spare(9);
+        game.move_spare(9);
+        game.move_spare(9);
+        game.move_spare(9);
+        game.move_spare(9);
+        game.move_spare(9);
+        game.final_frame_spare(9);
+        game.add_bonus_score(tab_score);
+        Assertions.assertEquals(190, game.final_score);
     }
-*/
 }
